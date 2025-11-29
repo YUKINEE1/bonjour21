91c484df72ff4729e391446254f5de3200cf0655
YUKINEE1 <fayssal.abaibat@usmba.ac.ma>
2025-11-29 19:02:33 +0100

    bonjour

---
+++ bonjour21/src/main/java/com/example/bonjour21/controller/ProductContoller.java
@@ -0,0 +1,34 @@
+package com.example.bonjour21.controller;
+
+import com.example.bonjour21.entity.Product;
+import com.example.bonjour21.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductContoller {
+    private ProductRepository productRepository;
+
+    public ProductContoller(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("products")
+    public List<Product> getallproduct(){
+        List<Product> products=this.productRepository.findAll();
+        return products;
+    }
+    @GetMapping("/products/{name}")
+    public Product getproductbyname(@PathVariable  String name){
+        Product produit= this.productRepository.findByName(name);
+        return produit;
+    }
+    @PostMapping
+    public Product createproduct(@RequestBody Product p){
+        Product pr=this.productRepository.save(p);
+        return pr;
+    }
+}
